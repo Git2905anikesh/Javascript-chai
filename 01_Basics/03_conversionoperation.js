38e757cbdca4e17d409c614389c28df2c478d16d
Anikesh soni <anikesh639414@gmail.com>
2025-09-23 11:59:04 +0000

    operaters

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -27,3 +27,36 @@ let sumNumber = 234
 let stringNumber = String(sumNumber)
 console.log(stringNumber);
 console.log(typeof (stringNumber));
+
+// *****************************OPERATIONS*************************
+
+let value = 23
+negvalue = -value
+console.log(negvalue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "hello"
+let str2 = " Anikesh"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+
+console.log(+true)
+
+let gamecounter = 1000
+gamecounter++
+console.log(gamecounter)
+
+
+// read about postfix and  prefix on MDN
