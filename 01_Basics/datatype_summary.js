c8f58ae5399a474ca92333812d702c91e60f82e1
Anikesh soni <anikesh639414@gmail.com>
2025-09-23 12:40:58 +0000

    datatype_summary

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,28 @@
+//  Primitive  =  7 type
+
+// 7 type = string, number, boolean , null, undefined , symbol, Bigint
+
+const score = 100
+const scorevalue = 1000.56
+const isloggedin = false
+const outsidetemp = null
+
+let useremail;
+
+
+// referance type or non primitives
+
+// Arrays, Objects , Function
+
+const heros = ["shakiman", "naagraaj"];
+let myobj = {
+    name : "anikesh" ,
+    age : 20
+}
+
+const myfunction = function(){
+    console.log("Hellow World");
+}
+
+console.log(typeof heros);
+
