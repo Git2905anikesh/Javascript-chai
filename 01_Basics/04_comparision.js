9731164415756dfd037e2302215ef2d27b66a425
Anikesh soni <anikesh639414@gmail.com>
2025-09-23 12:19:04 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(23==12)
+// console.log(23>12)
+// console.log(23<=12)
+// console.log(23<12)
+// console.log(23>=12)
+
+console.log("21" >  12)
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// ===
+
+console.log("23"===23)
+
