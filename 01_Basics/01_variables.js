3b22f42a7e2551a2215452067e228c6f63934fce
Anikesh soni <anikesh639414@gmail.com>
2025-09-23 05:53:13 +0000

    commit changes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid = 23432
+let accountEmail = "anikesh1225@gmail.com"
+var accountPassword = "355432"
+accountcity = "Mumbai"
+let accountstate;
+ 
+/*
+prefer not to use var becausse of its block scope and  functional scope issues
+*/
+ accountEmail = "pritam1225@gmail.com"
+ accountPassword = "1232342"
+ accountcity = "Chennai"
+console.table([accountid,accountEmail,accountPassword,accountcity,accountstate]);
